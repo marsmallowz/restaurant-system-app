190f3405c29b8ab41c454e305b8daa591a6e6e28
marsmallowz <alsandymaulana15@email.com>
2023-09-20 12:41:53 +0700

    add feature achiser and  kitchen

--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -1,5 +1,134 @@
-import React from "react";
+"use client";
+
+import PopUpOrder from "@/components/PopUpOrder";
+import React, { useEffect, useState } from "react";
+
+interface MenuItem {
+  id: string;
+  name: string;
+}
+
+interface OrderItem {
+  id: string;
+  tableId: number;
+  isCompleted: boolean;
+  menus: { menu: MenuItem; quantity: number }[];
+}
 
 export default function Cashier() {
-  return <div>Cashier</div>;
+  const [orders, setOrders] = useState<OrderItem[]>([]);
+  const [selectedOrder, setSelectedOrder] = useState<OrderItem>();
+  const [selectedTableId, setSelectedTableId] = useState<number>(0);
+  const [showPopUp, setShowPopUp] = useState<boolean>(false);
+
+  const handleClearTable = () => {
+    const newOrders = orders.filter(
+      (order) => order.tableId !== selectedTableId
+    );
+    const jsonData = JSON.stringify(newOrders);
+    localStorage.setItem("orders", jsonData);
+    setOrders(newOrders);
+    setSelectedTableId(0);
+    setSelectedOrder(undefined);
+    setShowPopUp(true);
+  };
+
+  useEffect(() => {
+    let getOrder = localStorage.getItem("orders") || undefined;
+    // ORDER
+    if (getOrder) {
+      const parsedMenu = JSON.parse(getOrder);
+      setOrders(parsedMenu);
+    } else {
+      const jsonData = JSON.stringify([]);
+      localStorage.setItem("orders", jsonData);
+      setOrders([]);
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div>Table</div>
+      <div className="flex justify-between">
+        <div className="flex w-7/12 gap-2">
+          <select
+            onChange={(e) => {
+              setSelectedTableId(parseInt(e.target.value));
+            }}
+            className="w-7/12 bg-white py-2 px-3 rounded-md"
+          >
+            <option key="" value="0">
+              Select Table
+            </option>
+            {orders
+              .sort((a, b) => a.tableId - b.tableId)
+              .map((order) => {
+                return (
+                  <option key={order.id} value={order.tableId}>
+                    Table {order.tableId}
+                  </option>
+                );
+              })}
+          </select>
+          <button
+            onClick={() => {
+              if (selectedTableId) {
+                const getOrder = orders.find(
+                  (order) => order.tableId === selectedTableId
+                );
+                setSelectedOrder(getOrder);
+              } else {
+                setSelectedOrder(undefined);
+              }
+            }}
+            className="w-4/12 p-2 bg-gray-500 text-white rounded-md text-sm"
+          >
+            Print Struck
+          </button>
+        </div>
+        <button
+          onClick={handleClearTable}
+          className={`${
+            selectedTableId ? "visible" : "invisible"
+          } w-3/12 bg-red-500 p-2 text-white rounded-md`}
+        >
+          Clear Table
+        </button>
+      </div>
+      {selectedOrder && (
+        <>
+          <table className="table-fixed text-left ">
+            <thead className="border-b">
+              <tr className="text-gray-400">
+                <th className="h-12 font-medium">Menu</th>
+                <th className="h-12 font-medium text-right">Quantity</th>
+                <th className="h-12 font-medium text-center">Price</th>
+              </tr>
+            </thead>
+            <tbody className="[&_tr:last-child]:border-0">
+              {selectedOrder &&
+                selectedOrder.menus.map((menu) => {
+                  return (
+                    <tr key={menu.menu.id} className="border-b">
+                      <td className="h-12">{menu.menu.name}</td>
+                      <td className="text-right">{menu.quantity}</td>
+                      <td className="text-center">Free</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+          <div className="text-center text-sm text-gray-500 font-medium">
+            Terima kasih sudah makan di{" "}
+            <span className="text-gray-600 font-bold">Restoran</span>
+          </div>
+        </>
+      )}
+      <PopUpOrder
+        visible={showPopUp}
+        setVisible={setShowPopUp}
+        message="Table successfully clear."
+      />
+    </div>
+  );
 }
--- src/app/kitchen/page.tsx
+++ src/app/kitchen/page.tsx
@@ -1,5 +1,53 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+interface MenuItem {
+  id: string;
+  name: string;
+}
+
+interface OrderItem {
+  id: string;
+  tableId: number;
+  isCompleted: boolean;
+  menus: { menu: MenuItem; quantity: number }[];
+}
 
 export default function Kitchen() {
-  return <div>Kitchen</div>;
+  const [orders, setOrder] = useState<OrderItem[]>([]);
+
+  useEffect(() => {
+    let getOrder = localStorage.getItem("orders") || undefined;
+    // ORDER
+    if (getOrder) {
+      const parsedMenu = JSON.parse(getOrder);
+      setOrder(parsedMenu);
+    } else {
+      const jsonData = JSON.stringify([]);
+      localStorage.setItem("orders", jsonData);
+      setOrder([]);
+    }
+  }, []);
+
+  return (
+    <div className="flex">
+      <div className="grid grid-cols-2 gap-2 w-full">
+        {orders.map((order) => {
+          return (
+            <div key={order.id} className="flex flex-col">
+              <div className="text-xl font-medium">Table {order.tableId}</div>
+              {order.menus.map((menu) => {
+                return (
+                  <div key={menu.menu.id} className="">
+                    {menu.quantity}x {menu.menu.name}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
